58e1848dfcb040ea5b446afdf9e99a95011164da
Josua Margandatua Hutapea <65341897+hotsince99@users.noreply.github.com>
2022-03-31 12:19:39 +0700

    initial commit

---
+++ src/PlaylistService.js
@@ -0,0 +1,37 @@
+const { Pool } = require('pg');
+
+class PlaylistService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylist(playlistId) {
+    const playlistQuery = {
+      text: `SELECT id, name
+             FROM playlists
+             WHERE playlists.id = $1`,
+      values: [playlistId],
+    };
+    const playlistResult = await this._pool.query(playlistQuery);
+
+    const songsQuery = {
+      text: `SELECT s.id, s.title, s.performer
+             FROM playlists
+             LEFT JOIN playlist_songs ps on playlists.id = ps.playlist_id
+             LEFT JOIN songs s on ps.song_id = s.id
+             WHERE playlist_id = $1`,
+      values: [playlistId],
+    };
+    const songsResult = await this._pool.query(songsQuery);
+
+    const playlist = playlistResult.rows[0];
+    playlist.songs = songsResult.rows;
+    return playlist;
+
+    // const album = albumResult.rows[0];
+    // album.songs = songsResult.rows;
+    // return album;
+  }
+}
+
+module.exports = PlaylistService;
